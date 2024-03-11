9bd5559471fb19f2ed7ac3f824d022c7fc05d7cb
kyeongb-bin <kyeongb0315@gmail.com>
2024-03-11 15:07:13 +0900

    이벤트 처리

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,18 +1,27 @@
-import styles from "./Hello.module.css";
-
 export default function Hello() {
+    function showName() {
+        console.log("Mike");
+    }
+    function showAge(age) {
+        console.log(age);
+    }
+
     return (
         <div>
-            <h1 style={{
-                    color: '#f00',
-                    borderRight: '2px solid #000',
-                    marginBottom: '50px',
-                    opacity: 1,
+            <h1>Hello</h1>
+            <button onClick={showName}>Show name</button>
+            <button 
+                onClick={() => {
+                    showAge(10)
                 }}
             >
-            Hello
-            </h1>
-            <div className={styles.box}>Hello</div>
+                Show age
+            </button>
+            <input type="text" 
+                onChange={e => {
+                    console.log(e.target.value);
+                }} 
+            />
         </div>    
     );
 }
